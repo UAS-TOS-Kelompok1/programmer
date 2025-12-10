551fee11b28740c5892b67a47fee092e3122fad7
nasrul eka p <110364259+nasedana@users.noreply.github.com>
2025-12-10 10:47:09 +0800

    Update laporan.php

--- laporan.php
+++ laporan.php
@@ -41,7 +41,7 @@ require 'sidebar.php'; ?>
                    <!-- DataTales Example -->
           <div class="card shadow mb-4">
             <div class="card-header py-3">
-              <h6 class="m-0 font-weight-bold text-primary">Daftar Karyawan</h6>
+              <h6 class="m-0 font-weight-bold text-primary">Laporan Keuangan</h6>
             </div>
             <div class="card-body">
               <div class="table-responsive">
